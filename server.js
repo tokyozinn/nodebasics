451c22900e68fc360f922ea279ff6cad178f0ad2
tokyozinn <andre.yabiku@hotmail.com>
2020-07-16 12:49:11 -0300

    atualizando pastas

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('../config/custom-express')
+const app = require('./src/config/custom-express')
 
 app.listen(3000, function () {
     console.log('Servidor rodando na porta 3000');
