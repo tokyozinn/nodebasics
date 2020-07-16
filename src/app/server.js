df45e06e2fc50d52feb12fd944aba216c8118fc7
tokyozinn <andre.yabiku@hotmail.com>
2020-07-16 12:10:22 -0300

    criando gitignore file

--- src/app/server.js
+++ src/app/server.js
@@ -1,32 +1,39 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function (req, resp) {
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000');
+})
 
-    let html = '';
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                </head>
-                <body>
-                    <h1>Casa do código</h1>
-                </body>
-            </html>    
-            `
-    } else if (req.url = '/livros') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                </head>
-                <body>
-                    <h1>Listagem de livros</h1>
-                </body>
-            </html>    
-            `
-    }
-    resp.end(html)
-});
+// const http = require('http');
 
-servidor.listen(3000);
+// const servidor = http.createServer(function (req, resp) {
+
+//     let html = '';
+//     if (req.url == '/') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="UTF-8">
+//                 </head>
+//                 <body>
+//                     <h1>Casa do código</h1>
+//                 </body>
+//             </html>    
+//             `
+//     } else if (req.url = '/livros') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="UTF-8">
+//                 </head>
+//                 <body>
+//                     <h1>Listagem de livros</h1>
+//                 </body>
+//             </html>    
+//             `
+//     }
+//     resp.end(html)
+// });
+
+// servidor.listen(3000);
